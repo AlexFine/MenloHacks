e7c256f4a6e18bbcac6bb0ac3526f5e5a2b14472
Kevin Hou <kevin.ch.hou@gmail.com>
2016-04-16 16:23:46 -0700

    Diffracted JS controllers

---
+++ UberScheduler/www/js/PlaylistsCtrl.js
@@ -0,0 +1,9 @@
+angular.module('playlistsCtrl', [])
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { time: '9:45 AM', id: 1, image: 'img/Golden.jpg', place: 'Golden Gate Bridge, San Francisco, CA'},
+    { time: '4:00 PM', id: 2, image: 'img/Art.jpg', place: '755 Ocean Ave, San Francisco, CA' },
+    { time: '6:30 PM', id: 3, image: 'img/Land.jpg', place: '680 Point Lobos Ave, San Francisco, CA 94121' },
+    { time: '5:00 AM', id: 4, image: 'img/Salesforce.jpg',  place: 'Salesforce Twoer, San Francisco, CA 94105' },
+  ];
+})
