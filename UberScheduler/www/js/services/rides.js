44b718c33b4aecea3503276f3913237b781c2e01
Kevin Hou <kevin.ch.hou@gmail.com>
2016-04-16 18:14:38 -0700

    Added service integration

--- UberScheduler/www/js/services/rides.js
+++ UberScheduler/www/js/services/rides.js
@@ -11,6 +11,16 @@ angular.module('ridesService', ['ionic'])
 
   })
 
+  .factory('retrieveSchedule', function() {
+    var playlists = [
+      { time: '9:45 AM', id: 1, image: 'img/Golden.jpg', dropoff: 'Golden Gate Bridge, San Francisco, CA', pickup: 'Menlo School, Atherton, CA 94027'},
+      { time: '4:00 PM', id: 2, image: 'img/Art.jpg', dropoff: '755 Ocean Ave, San Francisco, CA', pickup: 'Menlo School, Atherton, CA 94027' },
+      { time: '6:30 PM', id: 3, image: 'img/Land.jpg', dropoff: '680 Point Lobos Ave, San Francisco, CA 94121', pickup: 'Menlo School, Atherton, CA 94027' },
+      { time: '5:00 AM', id: 4, image: 'img/Salesforce.jpg',  dropoff: 'Salesforce Twoer, San Francisco, CA 94105', pickup: 'Menlo School, Atherton, CA 94027' },
+    ];
+    return playlists
+  })
+
   .factory('timeEstimate', function() {
     return function(long, lat, uber_level) {
       baseurl = "https://sandbox-api.uber.com/v1/estimates/time"
