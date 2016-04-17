a02888f4fb7cd4d973a6e6e22612f6c0f770cb67
Sam Schickler <gadgetsam@gmail.com>
2016-04-16 21:03:58 -0700

    added in database

--- UberScheduler/www/js/controllers/PlaylistsCtrl.js
+++ UberScheduler/www/js/controllers/PlaylistsCtrl.js
@@ -1,6 +1,6 @@
 // console.log("Playlists Ctrl loaded"); // Feedback
 angular.module('playlistsCtrl', ['ridesService'])
-.controller('PlaylistsCtrl', function($scope, testVariable, testFunction) {
+.controller('PlaylistsCtrl', function($scope, testVariable, testFunction,$http) {
   console.log(testFunction(5));
   console.log(testVariable);
 $scope.test = function($cordovaOauth){
@@ -11,6 +11,33 @@ $scope.test = function($cordovaOauth){
     console.log("Error -> " + error);
   });
 }
+  var url = "https://uberschedulerp.appspot.com/_ah/api/uberApi/v1/user/create";
+  $http.post(url, {
+    "userID":userID,
+    "message":email,
+    // "passwrd": storedUsername
+  }).then(function (resps) {
+    console.log(resps)
+  }
+  var url = "https://uberschedulerp.appspot.com/_ah/api/uberApi/v1/ride/return";
+  $http.post(url, {
+    "userID":userID
+  }).then(function (resps) {
+    console.log(resps)
+  }
+  var url = "https://uberschedulerp.appspot.com/_ah/api/uberApi/v1/ride/create";
+  $http.post(url, {
+    "daysOfWeek": "1",
+    "dropLat": 2,
+    "dropLong": 2,
+    "timeSec": "3",
+    "pickLat": 12,
+    "time": "2",
+    "pickLong": 3,
+    "userID": "sam"
+  }).then(function (resps) {
+    console.log(resps)
+  }
  $scope.playlists = [
     { time: '9:45 AM', id: 1, image: 'img/Golden.jpg', dropoff: 'Golden Gate Bridge, San Francisco, CA', pickup: 'Menlo School, Atherton, CA 94027'},
     { time: '4:00 PM', id: 2, image: 'img/Art.jpg', dropoff: '755 Ocean Ave, San Francisco, CA', pickup: 'Menlo School, Atherton, CA 94027' },
